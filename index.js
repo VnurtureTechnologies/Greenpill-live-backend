7a0d99b4c9e6c802fcf78ab2363c1793502a42d7
aaman123 <sutariyaaman1998@gmail.com>
2021-03-08 15:32:54 +0530

    dashboard and login views implemented

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const hbs = require('hbs');
+const path = require('path');
 
 var app = express();
+
+//common header and footer
+hbs.registerPartials(__dirname + '/views/common');
+
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 app.get('/', function(req,res) {
-    res.render("dashboard");
+    res.render("login/index");
+})
+
+app.get('/dashboard', function(req,res) {
+    res.render("dashboard/index")
 })
 
 const PORT = process.env.PORT || 8080;
