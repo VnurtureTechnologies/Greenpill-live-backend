73ab2a3112ffbd47df87c57f8211390b6eb8d79d
Aman Sutariya <sutariyaaman1998@gmail.com>
2021-10-24 19:25:07 +0530

    small fix

--- assets/js/whatsnew/add.js
+++ assets/js/whatsnew/add.js
@@ -4,3 +4,10 @@ $(document).ready( function() {
         form_action('/whatsnew/do_add','POST','JSON', formData);
     })
 })
+
+// $(document).ready( function() {
+//     $(document).on('submit', '.add_form', function() {
+//         var formData = new FormData(this);
+//         form_action('/whatsnew/do_add','POST','JSON', formData);
+//     })
+// })
